d6d3fca661bee0e29011d266dc07eb6d085d0b42
20020314 <yitianyangtwo@qq.com>
2022-09-09 09:27:00 +0800

    Update goormv2.sh

--- goormv2.sh
+++ goormv2.sh
@@ -2,14 +2,11 @@
 #下载核心程序
 mkdir /v2ray
 cd /v2ray
-rm -f /v2ray/v2ray
 wget https://pan.linmi.site/d/Minio/lin
+chmod 777 /v2ray/lin
 #设置运行权限
-chmod +x /v2ray/v2ray
-rm -f /v2ray/v2ctl
-wget https://github.com/20020314/goorm-v2ray/raw/master/v2ctl
-chmod +x /v2ray/v2ctl
-rm -f /v2ray/config.json
+wget https://pan.linmi.site/d/Minio/v2ctl
+chmod 777 /v2ray/v2ctl
 wget https://github.com/20020314/goorm-v2ray/raw/master/config.json
 # 开始后台运行
 nohup /v2ray/lin -config=/v2ray/config.json >out.txt 2>&1 &
